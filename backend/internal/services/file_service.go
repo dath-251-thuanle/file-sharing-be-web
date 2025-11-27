f54499c81bca151cc62a7e27df7d8f2fa6464369
root <root@Kkanger.localdomain>
2025-11-27 11:45:59 +0000

    add helper function

--- backend/internal/services/file_service.go
+++ backend/internal/services/file_service.go
@@ -41,13 +41,34 @@ type UploadInput struct {
 	AvailableTo   *time.Time
 }
 
-func (in *UploadInput) container() storage.ContainerType {}
+func (in *UploadInput) container() storage.ContainerType {
+	if in != nil && in.IsPublic != nil && *in.IsPublic {
+		return storage.ContainerPublic
+	}
+	return storage.ContainerPrivate
+}
 
-func (in *UploadInput) sanitizedFileName() string {}
+func (in *UploadInput) sanitizedFileName() string {
+	if in == nil || in.FileName == "" {
+		return uuid.NewString()
+	}
+	name := filepath.Base(in.FileName)
+	name = strings.TrimSpace(name)
+	if name == "" || name == "." {
+		return uuid.NewString()
+	}
+	return name
+}
 
 func (s *FileService) UploadFile(ctx context.Context, input *UploadInput) (*models.File, error) {}
 
-func optionalString(val string) *string {}
+func optionalString(val string) *string {
+	if val == "" {
+		return nil
+	}
+	v := val
+	return &v
+}
 
 func containerFromFile(file *models.File) storage.ContainerType {}
 
